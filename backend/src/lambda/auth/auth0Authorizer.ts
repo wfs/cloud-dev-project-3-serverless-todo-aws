ebd2b6b04d2e850d396ad094d6008314c6ffebf1
wfs <confirmalert@gmail.com>
2020-01-11 14:37:19 +1100

    project submission

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -16,7 +16,26 @@ const logger = createLogger("auth");
 // const cert = await Axios.get(jwksUrl).promise()
 
 // #TODO
-const cert = `...`;
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDDzCCAfegAwIBAgIJOjUl6QS0sMuyMA0GCSqGSIb3DQEBCwUAMCUxIzAhBgNV
+BAMTGnRlc3QtZW5kcG9pbnQuYXUuYXV0aDAuY29tMB4XDTIwMDEwMTAwNDMwNFoX
+DTMzMDkwOTAwNDMwNFowJTEjMCEGA1UEAxMadGVzdC1lbmRwb2ludC5hdS5hdXRo
+MC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCYm6gYQMqCeYtk
+NsFpQ8itkj4TUG0uDQwW3VMmdxMZstxAavG2agEDRdLF5vr/SfFiPOfmc4KuXXS9
+8OlMVruwqI/I0MiNRj5uZAKYzgzuQIdc5bTuToNHxoa5EYyuBDP8J9Tv3NFvaLGe
+H8SeAHzQs8KkjsgAEArivIEGMCsy7E6yACy3GpCncz390fNIu1xzH39xYRvdeosI
+9fS0aprw6ElfDJBekd1ax5N0m8kZVEnrQC69OuHFwTqRrvbZAC65wje8i54M9z0f
+It83TLPcDt/3RMMLzoxML+GEzfdpD4BNX8K9x+hD1AAIHPS9ADn9gfswUUrQFex1
+nRfoP6tXAgMBAAGjQjBAMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFDGdj2au
+mf036mDpz0U0fq56YcprMA4GA1UdDwEB/wQEAwIChDANBgkqhkiG9w0BAQsFAAOC
+AQEADwmyDb5fc+PnpRouALLnpugfgkYvzX1siIcf5sVVJ5xSg2D4vemjvZKdk9fh
+nbSa7feHc8srM9fWpgNYFru09UDHa/J1NTanRgaH9k7yjonAjnAMYdkpmhet9EPT
+gLonRSs42Ql/mnBPVJmpz9eL5G2I10xcU7brvfnelEE00XeFPDmCdGHplTMExFLk
+09u6M9iF/itIqfRy4ZViSroRs43tzXhVDjgUkKRxL/eDNoocn3ZiutQWcWaan8qp
+Nx6XPdABvaS+SQf6T6kyFp7WYcyRJAUr2vwm0NSWAEzWkQK+yqe69OIX4tR27Jdo
+5y/GLK41r2SurjE1tiay8mUGLQ==
+-----END CERTIFICATE-----
+`;
 
 export const handler = async (
   event: CustomAuthorizerEvent
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'nyxzfq97mk'
 export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'test-endpoint.au.auth0.com', // Auth0 domain
-  clientId: 'Sb3JXMshveVusf4r9ShKG15FIfd5yu7R', // Auth0 client id
+  clientId: 'z646LnfKNVa2ijUWOIOmcTb5Fu0tyz5L', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
